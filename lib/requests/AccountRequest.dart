066481ca2ad02c57b26151e928b41acd2ab05c37
MARIN HUGO p2007617 <hugo.marin@etu.univ-lyon1.fr>
2023-05-10 19:22:01 +0200

    ajout getFriendsPosts() + retraits multiples print()

--- lib/requests/AccountRequest.dart
+++ lib/requests/AccountRequest.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'server_ip.dart';
 
 class AccountRequest {
-  static const String url = 'http://${serverIp}:5000/api/auth/';
+  static const String url = 'http://$serverIp:5000/api/auth/';
 
   static Future<http.Response> login(String phone, String password) async {
     http.Response res = await http.post(Uri.parse('${url}login'),
@@ -21,8 +22,11 @@ class AccountRequest {
   }
 
   static Future<http.Response> delete(String id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final cookie = prefs.getString('cookie') ?? '';
+
     http.Response res = await http.post(Uri.parse('$url/api/auth/deleteUser'),
-        headers: {'Content-type': 'application/json'},
+        headers: {'Content-type': 'application/json', 'cookie': cookie},
         body: jsonEncode({'id': id}));
 
     return res;
--- lib/requests/PostRequests.dart
+++ lib/requests/PostRequests.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:html';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'server_ip.dart';
@@ -11,8 +12,19 @@ class PostRequests {
     final cookie = prefs.getString('cookie') ?? '';
 
     http.Response res = await http.post(Uri.parse('${url}createPost'),
-        headers: {'Content-type': 'application/json', 'set-cookie': cookie},
+        headers: {'Content-type': 'application/json', 'cookie': cookie},
         body: jsonEncode({"image": image}));
+
+    return res;
+  }
+
+  static Future<http.Response> getFriendsPosts() async {
+    final prefs = await SharedPreferences.getInstance();
+    final cookie = prefs.getString('cookie') ?? '';
+
+    http.Response res = await http.get(Uri.parse('${url}getFriendsPosts'),
+        headers: {'content-type': 'application/json', 'cookie': cookie});
+
     return res;
   }
 }
